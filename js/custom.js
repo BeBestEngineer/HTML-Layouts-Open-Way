ec29bee8d73486b01f723b236280aaf633efe5d5
Alexandr Gorchakov <385898sw@gmail.com>
2016-11-26 11:13:35 +0600

    normalize offset for scroll to section

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    $(".scroll-to-item").click( function() {
-        console.log( $(this).attr('href') );    
-      $(window).scrollTo( $(this).attr('href'), 1000, {offset: {top:-50, left:0} });
-    });
+    $.extend($.scrollTo.defaults, {        
+        offset: -50
+    });   
 });
